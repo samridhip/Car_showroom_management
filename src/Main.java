e0627db2bb89f08c17985fe06f93375fa14bbcf5
samridhip <pandeysamridhi1@gmail.com>
2024-01-08 23:49:22 +0530

    Car Showroom Management projects

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+interface utility{
+    public void get_details();
+    public void set_details();
+}
+public class Main {
+    static void main_menu(){
+        System.out.println();
+        System.out.println("==============******WELCOME TO SHOWROOM MANAGEMENT SYSTEM******==============");
+        System.out.println();
+        System.out.println("==============***ENTER YOUR CHOICE***================");
+        System.out.println();
+        System.out.println("1].ADD SHOWROOM \t\t\t 2].ADD EMPLOYEES \t\t\t 3]ADD CARS");
+        System.out.println();
+        System.out.println("4].GET SHOWROOM \t\t\t 5].GET EMPLOYEES \t\t\t 6].GET CARS");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=
+    }
+}
