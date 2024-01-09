5afca54b38e5cebfa0c55e8033f3b315f68f9ea8
samridhip <pandeysamridhi1@gmail.com>
2024-01-09 23:21:23 +0530

    Car Showroom Management projects

--- src/Employees.java
+++ src/Employees.java
@@ -1,2 +1,12 @@
-public class Employees {
+public class Employees extends Showroom implements utility{
+    String emp_id;
+    String emp_name;
+    int emp_age;
+    String emp_department;
+    @Override
+    public void get_details(){
+        System.out.println("ID: "+emp_id);
+        
+
+    }
 }
